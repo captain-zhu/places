b21096b58d51873893e77cac67aa58607094b5d5
captain-zhu <andypaker@qq.com>
2016-02-14 21:44:06 +0800

    In Place class fix initialize method bug and create destroy method

--- app/models/place.rb
+++ app/models/place.rb
@@ -20,7 +20,7 @@ class Place
   end
 
   def initialize params
-    @id = params[:_id].to_s
+    @id = params[:_id].nil?? params[:id] : params[:_id].to_s
     address_components = params[:address_components]
     @address_components = Array.new
     address_components.each do |address_component|
@@ -33,7 +33,7 @@ class Place
 
   #ﬁnd all documents in the places collection with a matching address_components.short_name
   def self.find_by_short_name short_name
-    self.collection.find("address_components.short_name" => short_name)
+    self.collection.find(:"address_components.short_name" => short_name)
   end
 
   #accept a Mongo::Collection::View and return a collection of Place instances.
@@ -47,7 +47,8 @@ class Place
   #return an instance of Place for a supplied id
   def self.find id
     object_id = BSON::ObjectId.from_string id
-    Place.new self.collection.find("_id" => object_id).first
+    document = self.collection.find(:_id => object_id).first
+    return document.nil?? nil : Place.new(document)
   end
 
   #return an instance of all documents as Place instances
@@ -55,5 +56,9 @@ class Place
     self.to_places self.collection.find().skip(offset).limit(limit)
   end
 
-
+  #delete the document associtiated with its assigned id.
+  def destroy
+    object_id = BSON::ObjectId.from_string(@id)
+    self.class.collection.find(:_id => object_id).delete_one
+  end
 end
