41411e091bd151fc7509dd47147b17189220a090
captain-zhu <andypaker@qq.com>
2016-02-15 09:51:20 +0800

    Photo class add initialize method

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,16 @@
 class Photo
   include Mongoid::Document
-
   attr_accessor :id, :location
   attr_writer :contents
 
   # returns a MongoDB Client from Mongoid referencing the default database from the config/mongoid.yml ﬁle
   def mongo_client
     Mongoid::Clients.default
+  end
+
+  def initialize hash={}
+    @id = hash[:_id].to_s unless hash[:_id].nil?
+    @location = Point.new hash[:metadata][:location] unless hash[:metadata].nil?
   end
 
 
